7c47224b8d4e975d63c6cf6257908575fc7c4d89
Esther <11yekinie@gmail.com>
2025-02-09 16:29:49 +0000

    connection to db

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 // handle using the correct environment variables here
 
@@ -6,6 +7,9 @@ if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
 }
 
-// or you can just export pool like below if you do not know the information above
-
-module.exports = new Pool();
+module.exports = new Pool({
+    connectionString: process.env.DATABASE_URL_EXTERNAL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
